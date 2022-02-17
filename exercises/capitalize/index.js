ca6840047632eb0b5eb74be0e331df5b13776c5a
Enes Karakaş <enes.karakas.e@gamil.com>
2022-02-17 11:42:11 +0300

    capitalize done

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  return str
+    .split(' ')
+    .map((word) => {
+      const firstLetter = word[0].toUpperCase();
+      const others = word.slice(1);
+      return `${firstLetter}${others}`;
+    })
+    .join(' ');
+}
 
 module.exports = capitalize;
