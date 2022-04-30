c27607501487162f5970310ae5c1063f920687dd
Enes Karakaş <enes.karakas.e@gmail.com>
2022-04-30 17:55:52 +0300

    vowels covered

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,24 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  // solution 2 regex
+  const vowels = /[aeiou]/gi;
+
+  const a = str.match(vowels);
+
+  return a ? a.length : 0;
+
+  // solution 1
+  // const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  // return str
+  //   .toLowerCase()
+  //   .split('')
+  //   .map((letter) => {
+  //     return vowels.includes(letter);
+  //   })
+  //   .filter((n) => n).length;
+}
 
 module.exports = vowels;
