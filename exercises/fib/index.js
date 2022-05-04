f1125e9693b2af50e06b235dc74aa7272a629114
Enes Karakaş <enes.karakas.e@gmail.com>
2022-05-04 20:31:26 +0300

    fib with time complexity issue, memorized covered awesome

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,58 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// memorize solution!
+
+function memorize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const r = fn.apply(this, args);
+    //alternatives
+    // const r = fn.call(this, ...args);
+    // const r = fn(...args);
+
+    cache[args] = r;
+
+    return r;
+  };
+}
+
+function slowFib(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
+}
+
+const fib = memorize(slowFib);
+
+//solution recursive!
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+// //solution iteration!
+// function fib(n) {
+//   const arr = [0, 1];
+//   if (n === 0 || n === 1) {
+//     return arr[n];
+//   }
+
+//   for (let i = 2; i < n + 1; i++) {
+//     const genN = arr[i - 1] + arr[i - 2];
+//     arr.push(genN);
+//   }
+
+//   return arr[n];
+// }
 
 module.exports = fib;
