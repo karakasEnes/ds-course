61751aa7c228b8a3b286980d47075b06b9ce4000
Enes Karakaş <enes.karakas.e@gmail.com>
2022-05-05 15:33:32 +0300

    queue covered

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,18 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.myArr = [];
+  }
+
+  add(n) {
+    this.myArr.unshift(n);
+  }
+
+  remove() {
+    return this.myArr.pop();
+  }
+}
 
 module.exports = Queue;
