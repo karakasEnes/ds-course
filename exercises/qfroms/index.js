a9d4f57192aecd365c510c155a650863c49e1ab9
Enes Karakaş <enes.karakas.e@gmail.com>
2022-05-06 19:02:07 +0300

    qfroms with stack covered nice one

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,56 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stackOne = new Stack();
+    this.stacktTwo = new Stack();
+  }
+
+  _moveFromAtoB() {
+    while (this.stackOne.peek()) {
+      const popedItem = this.stackOne.pop();
+      this.stacktTwo.push(popedItem);
+    }
+  }
+
+  _moveFromBtoA() {
+    while (this.stacktTwo.peek()) {
+      const popedItem = this.stacktTwo.pop();
+      this.stackOne.push(popedItem);
+    }
+  }
+
+  add(n) {
+    return this.stackOne.push(n);
+  }
+
+  peek() {
+    //move from a to b
+    this._moveFromAtoB();
+
+    //return top element inside B with peek
+    const lastItem = this.stacktTwo.peek();
+
+    //move them back position
+    this._moveFromBtoA();
+
+    //return lastitem
+    return lastItem;
+  }
+
+  remove() {
+    //move from a to b
+    this._moveFromAtoB();
+
+    //remove last item from B
+    const removedItem = this.stacktTwo.pop();
+
+    //move things from b to a
+    this._moveFromBtoA();
+
+    return removedItem;
+  }
+}
 
 module.exports = Queue;
