ba4fce87e2a04ebc236bcd8dd64f161330c74681
Enes Karakaş <enes.karakas.e@gmail.com>
2022-05-08 12:31:09 +0300

    linked List, insertFirst, size methods

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,32 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let counter = 0;
+    let current = this.head;
+    while (current) {
+      counter++;
+      current = current.next;
+    }
+
+    return counter;
+  }
+}
 
 module.exports = { Node, LinkedList };
