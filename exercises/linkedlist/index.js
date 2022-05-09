874970c90b6b0f006d717781c59c2a1b4eb20015
Enes Karakaş <enes.karakas.e@gmail.com>
2022-05-09 14:39:18 +0300

    clear, removefirst, removelast

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -42,6 +42,50 @@ class LinkedList {
 
     return curNode ? curNode : null;
   }
+
+  clear() {
+    this.head = null;
+  }
+
+  removeFirst() {
+    if (!this.head) return null;
+
+    //long solution
+    // const secondNode = this.head.next;
+
+    // if (!secondNode) {
+    //   this.head = null;
+    // } else {
+    //   this.head = secondNode;
+    // }
+
+    //better solution
+    this.head = this.head.next;
+  }
+
+  removeLast() {
+    if (!this.head) return null;
+
+    //if only 1 node in list
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+
+    // if only 2 node in list?
+
+    // other cases!
+
+    let prevNode = this.head;
+    let curNode = this.head.next;
+
+    while (curNode.next) {
+      prevNode = curNode;
+      curNode = curNode.next;
+    }
+
+    prevNode.next = null;
+  }
 }
 
 module.exports = { Node, LinkedList };
