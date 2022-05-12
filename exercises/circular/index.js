cc391a782cb6eb86b1ba1cd9d8e987cf2369f21f
Enes Karakaş <enes.karakas.e@gmail.com>
2022-05-12 17:11:13 +0300

    circular of linked list problem

--- exercises/circular/index.js
+++ exercises/circular/index.js
@@ -12,6 +12,22 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+  if (!list.head) return;
+  if (list.size() <= 2) return;
+
+  let slow = list.head;
+  let fast = list.head;
+
+  while (slow.next && fast.next.next) {
+    if (slow === fast) {
+      return true;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return false;
+}
 
 module.exports = circular;
