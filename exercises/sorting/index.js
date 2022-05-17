10ff177b1f9dc0e2bac07467a187599ef47ddbb5
Enes Karakaş <enes.karakas.e@gmail.com>
2022-05-18 01:58:03 +0300

    selectionSort and mergeSort ( hardone )

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -16,10 +16,47 @@ function bubbleSort(arr) {
   return arr;
 }
 
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  let lenArr = arr.length;
+  for (let i = 0; i < lenArr; i++) {
+    let indexOfMin = i;
+    for (let j = i + 1; j < lenArr; j++) {
+      if (arr[j] < arr[indexOfMin]) {
+        indexOfMin = j;
+      }
+    }
 
-function mergeSort(arr) {}
+    if (arr[indexOfMin] === arr[i]) continue;
+    let temp = arr[i];
+    arr[i] = arr[indexOfMin];
+    arr[indexOfMin] = temp;
+  }
 
-function merge(left, right) {}
+  return arr;
+}
+
+function mergeSort(arr) {
+  if (arr.length === 1) return arr;
+
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] > right[0]) {
+      results.push(right.shift());
+    } else {
+      results.push(left.shift());
+    }
+  }
+
+  return [...results, ...left, ...right];
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
--- exercises/sorting/test.js
+++ exercises/sorting/test.js
@@ -18,13 +18,13 @@ describe('Bubble sort', () => {
   });
 });
 
-describe.skip('Selection sort', () => {
+describe('Selection sort', () => {
   test('sorts an array', () => {
     expect(selectionSort(getArray())).toEqual(getSortedArray());
   });
 });
 
-describe.skip('Merge sort', () => {
+describe('Merge sort', () => {
   test('merge function can join together two sorted arrays', () => {
     const left = [1, 10];
     const right = [2, 8, 12];
